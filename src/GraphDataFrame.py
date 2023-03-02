586a3c4c06c3013554ed93a1cb54753db3f2a434
JuchengHu <36852779+smgjch@users.noreply.github.com>
2023-03-02 16:01:03 +0000

    add point highlight

--- src/GraphDataFrame.py
+++ src/GraphDataFrame.py
@@ -62,6 +62,19 @@ class Plot():
         
         """
         plt.plot(self.x,self.y)
+        self.highlight()
         plt.xlabel(f'Loss {self.event}')
         plt.ylabel(f'Return period {self.country}')
         plt.show()
+
+    def highlight(self):
+        start_point = 1
+        end = 10
+        while True:
+            if start_point<(self.x.max()-self.x.min())+1 and start_point < end:
+
+                highlight_point_x = np.interp(start_point, self.y, self.x)
+                plt.plot(highlight_point_x, start_point, 'ro')
+                start_point += 2
+            else:
+                return
