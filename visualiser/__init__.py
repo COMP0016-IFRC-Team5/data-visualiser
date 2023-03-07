6ea34b907ddcb7257da3b32439af56471f38c3d9
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-07 13:43:00 +0000

    feat: placeholder for `get_available_countries`

--- visualiser/__init__.py
+++ visualiser/__init__.py
@@ -1,8 +1,9 @@
-from ._main import set_data_folder, get_data_folder
+from ._main import set_data_folder, get_data_folder, get_available_countries
 from ._model import Loss
 
 __all__ = [
     'set_data_folder',
     'get_data_folder',
+    'get_available_countries',
     'Loss',
 ]
--- visualiser/_main.py
+++ visualiser/_main.py
@@ -10,7 +10,12 @@ def get_data_folder():
     return _config.get_data_folder()
 
 
+def get_available_countries():
+    ...
+
+
 __all__ = [
     'set_data_folder',
     'get_data_folder',
+    'get_available_countries',
 ]
