eef6f954bad17808e5dda088b0e64fc9ec334db8
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-07 13:51:00 +0000

    feat: placeholder for `plot_exceedance_curves`

--- visualiser/__init__.py
+++ visualiser/__init__.py
@@ -1,9 +1,11 @@
-from ._main import set_data_folder, get_data_folder, get_available_countries
+from ._main import set_data_folder, get_data_folder, get_available_countries, \
+    plot_exceedance_curves
 from ._model import Loss
 
 __all__ = [
     'set_data_folder',
     'get_data_folder',
     'get_available_countries',
+    'plot_exceedance_curves',
     'Loss',
 ]
--- visualiser/_main.py
+++ visualiser/_main.py
@@ -14,8 +14,13 @@ def get_available_countries():
     ...
 
 
+def plot_exceedance_curves(countries, events, losses):
+    ...
+
+
 __all__ = [
     'set_data_folder',
     'get_data_folder',
     'get_available_countries',
+    'plot_exceedance_curves',
 ]
