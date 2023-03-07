752b5b1f4034f7e839bfce9035e6e7285a9bd52e
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-07 12:25:51 +0000

    feat: example

--- example.py
+++ example.py
@@ -1,5 +1,22 @@
+from enum import Enum
 import visualiser
 
 
+class Loss(Enum):
+    deaths = 1
+    affected_people = 2
+
+
 if __name__ == '__main__':
-    visualiser.set_data_folder('data')
+    visualiser.set_data_folder('./data')
+    print(visualiser.available_countries)
+    countries = ["Lebanon", "Spain"]
+    country = 'Mexico'
+    events = ["Floods", "eArthQuaKEs"]  # case insensitive
+    event = 'FLOODS'
+    visualiser.plot_exceedance_curves(countries, events, [Loss.deaths,
+                                                          Loss.affected_people])
+    visualiser.plot_exceedance_curves(country, event, Loss.deaths)
+    table = visualiser.get_exceedance_table(country, event)
+    tables = visualiser.get_exceedance_table(countries, events)
+
--- visualiser/__init__.py
+++ visualiser/__init__.py
@@ -1,1 +1,3 @@
 from ._config import set_data_folder, get_data_folder
+
+__all__ = ['set_data_folder', 'get_data_folder']
