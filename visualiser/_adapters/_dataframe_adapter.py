2d5d47bd20739861038e1f830e99df3a8f3ddb68
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-07 17:54:35 +0000

    fix: attribute error

--- visualiser/_adapters/_dataframe_adapter.py
+++ visualiser/_adapters/_dataframe_adapter.py
@@ -7,5 +7,5 @@ __all__ = ["DataFrameAdapter"]
 
 class DataFrameAdapter:
     def __init__(self, country: Directory, event: str):
-        file = country.find_file(event)
+        file = country.find_file(f"{event}.csv")
         self.dataframe = pd.read_csv(file.get_filepath())
