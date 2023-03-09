ec49c8f49899c60bab13f01e0bfdf09f8f74bf84
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 16:43:03 +0000

    docs: dataframe adapter

--- visualiser/_adapters/_dataframe_adapter.py
+++ visualiser/_adapters/_dataframe_adapter.py
@@ -6,6 +6,18 @@ __all__ = ["DataFrameAdapter"]
 
 
 class DataFrameAdapter:
+    """A class for reading event data into a Pandas DataFrame.
+
+    Attributes:
+        dataframe (pd.DataFrame): The Pandas DataFrame containing the event
+            data.
+
+    Args:
+        country (Directory): The directory representing the country where the
+            event occurred.
+        event (str): The name of the event to read.
+
+    """
     def __init__(self, country: Directory, event: str):
         file = country.find_file(f"{event}.csv")
         self.dataframe = pd.read_csv(file.get_filepath())
