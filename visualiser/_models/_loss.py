de293259b74ed29aabf3de58f7c3f57e69cf8d38
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 16:46:54 +0000

    docs: docstrings for loss

--- visualiser/_models/_loss.py
+++ visualiser/_models/_loss.py
@@ -4,8 +4,22 @@ __all__ = ["Loss"]
 
 
 class Loss(Enum):
+    """An enumeration of types of losses.
+
+    Each member of the enumeration represents a different type of loss.
+
+    """
     deaths = "Deaths"
     affected_people = "Affected People"
 
     def __eq__(self, other):
+        """Check if two Losses have the same value.
+
+        Args:
+            other (Loss): The other Loss to compare against.
+
+        Returns:
+            bool: True if the Losses have the same value; False otherwise.
+
+        """
         return self.value == other.value
