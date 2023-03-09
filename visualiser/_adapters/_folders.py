596b6d66dd3490941b985de2b185226a8c612d1d
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 16:45:49 +0000

    docs: folders

--- visualiser/_adapters/_folders.py
+++ visualiser/_adapters/_folders.py
@@ -2,8 +2,22 @@ from enum import Enum
 
 
 class Folders(Enum):
+    """An enumeration of folder names.
+
+    Each member of the enumeration represents the name of a specific folder.
+
+    """
     unsliced = "unsliced_data_sheets"
     sliced = "sliced_data_sheets"
 
     def __eq__(self, other):
+        """Check if two Folders have the same value.
+
+        Args:
+            other (Folder): The other Folder to compare against.
+
+        Returns:
+            bool: True if the Folders have the same value; False otherwise.
+
+        """
         return self.value == other.value
