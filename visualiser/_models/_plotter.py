c84fadb0115255f5d18bfec1b0be7aef2a0cd1c5
Dekun Zhang <91729508+DekunZhang@users.noreply.github.com>
2023-03-09 16:54:28 +0000

    docs: docstrings for plotter

--- visualiser/_models/_plotter.py
+++ visualiser/_models/_plotter.py
@@ -8,6 +8,32 @@ __all__ = ['Plotter']
 
 
 class Plotter:
+    """
+    A class that plots the return period curve and highlights certain points
+    with their corresponding loss values.
+
+    Args:
+        country (str): Country name
+        event (str): Event name
+        x (pandas.Series): Loss values
+        y (pandas.Series): Return period values
+        loss (str): Type of loss
+
+    Attributes:
+        __x (pandas.Series): Loss values
+        __y (pandas.Series): Return period values
+        __loss (str): Type of loss
+        __country (str): Country name
+        __event (str): Event name
+        __table (pandas.DataFrame): A table containing highlighted points
+            and their corresponding loss and return period values.
+
+    Methods:
+        plot: Plots the return period curve and highlights certain points
+            with their corresponding loss values.
+        get_table: Returns a table containing highlighted points and their
+            corresponding loss and return period values.
+    """
     def __init__(self, country: str, event: str, x, y, loss: str):
         self.__x = x
         self.__y = y
@@ -21,6 +47,18 @@ class Plotter:
             sliced: bool = False,
             required_years: int = -1
     ):
+        """
+        Plots the return period curve and highlights certain points
+        with their corresponding loss values.
+
+        Args:
+            show_graph (bool, optional): Whether to show the graph or not.
+                Defaults to True.
+            sliced (bool, optional): Whether the data is sliced or not.
+                Defaults to False.
+            required_years (int, optional): A return period value to
+                highlight. Defaults to -1.
+        """
         line, = plt.plot(self.__x, self.__y)
         plt.xlabel(self.__loss)
         plt.ylabel('Return period (years)')
@@ -35,6 +73,14 @@ class Plotter:
 
     @staticmethod
     def __add_label(line, sliced, required_years):
+        """
+        Adds label to the plotted curve based on the given input values.
+
+        Args:
+            line: A plotted line.
+            sliced (bool): Whether the data is sliced or not.
+            required_years (int): A return period value to highlight.
+        """
         if sliced and required_years != -1:
             line.set_label(f'{required_years}_sliced')
 
@@ -48,15 +94,35 @@ class Plotter:
             line.set_label("unsliced")
 
     def __is_data_sufficient(self):
+        """
+        Checks if the given data is sufficient for plotting or not.
+
+        Returns:
+            bool: Whether the given data is sufficient or not.
+        """
         return self.__y.nunique() >= 4 or self.__y.max() >= 10
 
     def __highlight(self, plot):
+        """
+        Highlights certain points with their corresponding loss values
+        and creates a table.
+
+        Args:
+            plot: The current plot object.
+        """
         start_point = 1 if self.__y.min() > 0 else round(self.__y.min())
         end = 10 if self.__y.max() > 10 or math.isnan(self.__y.max()) \
             else round(self.__y.max())
         accepted_return_periods = [1, 3, 5, 10]
 
         def highlight_point(start_return_period):
+            """Recursively highlights points in the plot and adds corresponding
+            data to the table.
+
+            Args:
+                start_return_period (int): The return period to start
+                highlighting from.
+            """
             if start_return_period > end:
                 return
             if start_return_period not in accepted_return_periods:
@@ -83,4 +149,12 @@ class Plotter:
         highlight_point(start_point)
 
     def get_table(self):
+        """
+        Returns a table containing highlighted points and their
+        corresponding loss and return period values.
+
+        Returns:
+            pandas.DataFrame: A table containing highlighted points and their
+            corresponding loss and return period values.
+        """
         return self.__table
