e051b2c3b699b2d38cc02d7875e92cf3cfe74693
JuchengHu <36852779+smgjch@users.noreply.github.com>
2023-03-09 15:12:04 +0000

    example trim 2

--- example.py
+++ example.py
@@ -4,25 +4,25 @@ if __name__ == '__main__':
     visualiser.set_data_folder('./data')
     print(visualiser.get_available_countries())
     countries = ["Albania", "Pakistan"]
-    #country = "Mexico"
-    #country = "Belize"
+    # country = "Mexico"
+    # country = "Belize"
     events = ["Floods",
               "eArthQuaKEs"]  # case insensitive
     # event = 'Floods'
-    #event = 'Storms'
-    # visualiser.plot_exceedance_curves(
-    #     countries,
-    #     events,
-    #     [visualiser.Loss.deaths, visualiser.Loss.affected_people],
-    #     15
-    # )
-    visualiser.plot_exceedance_curves(country, event,
-                                      visualiser.Loss.deaths, 15)
+    # event = 'Storms'
+    visualiser.plot_exceedance_curves(
+        countries,
+        events,
+        [visualiser.Loss.deaths, visualiser.Loss.affected_people],
+        15
+    )
+    #visualiser.plot_exceedance_curves(country, event,
+    #                                  visualiser.Loss.deaths, 15)
     #table = visualiser.get_exceedance_table(country, event, 15)
-    tables = visualiser.get_exceedance_table(countries, events)
-    for _country in table:
-        for _event in table[_country]:
-            df = table[_country][_event]
+    tables = visualiser.get_exceedance_table(countries, events,15)
+    for _country in tables:
+        for _event in tables[_country]:
+            df = tables[_country][_event]
             print(_country, _event)
             print(df)
             print("--------------------")
